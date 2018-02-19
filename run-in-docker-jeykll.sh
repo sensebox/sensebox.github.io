79e451bbd3f4f8ace2a69a3e61193bd6bb2b5dec
ubergesundheit <ubergesundheit@googlemail.com>
2018-02-19 11:48:01 +0100

    fix running the repo in docker

--- run-in-docker-jeykll.sh
+++ run-in-docker-jeykll.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/srv/jekyll -p 4000:4000 jekyll/jekyll:pages jekyll s
+docker run --rm -it -v /dev/null:/srv/jekyll/Gemfile -v $(pwd):/srv/jekyll -p 4000:4000 jekyll/jekyll:pages jekyll s
