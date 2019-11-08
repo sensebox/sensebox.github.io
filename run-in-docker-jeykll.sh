4a288a4b6b104a09398c46f16bcacfd3cd00dc7f
felixerdy <felix.erdmann@uni-muenster.de>
2019-11-08 11:51:27 +0100

    products page including mini

--- run-in-docker-jeykll.sh
+++ run-in-docker-jeykll.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it -v /dev/null:/srv/jekyll/Gemfile -v $(pwd):/srv/jekyll -p 4000:4000 jekyll/jekyll:pages jekyll s
+#docker run --rm -it -v /dev/null:/srv/jekyll/Gemfile -v $(pwd):/srv/jekyll -p 4000:4000 jekyll/jekyll:pages jekyll s
+docker run --rm -it -v $(PWD):/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll serve --livereload
